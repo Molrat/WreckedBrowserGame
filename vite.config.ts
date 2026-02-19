c3a90cabbbcb845cadb5e59d4f23d70da55d5974
Molrat <j.somers301@gmail.com>
2026-02-19 12:02:23 +0100

    fix vite config bug for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import path from 'node:path'
 
 export default defineConfig({
+  base: '/WreckedBrowserGame/',  // ← YOUR REPO NAME HERE
+
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'src'),
     },
   },
+
   server: {
     port: 5173,
     open: true,
   },
-});
+})
